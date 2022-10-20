77c0ccae8480e5f6283641596c628c251a8f70f8
Kisiara Frank <franciskisiara@gmail.com>
2022-10-20 16:37:57 +0300

    added the base for fetching a list of countries and having them ... you know ... working

--- database/migrations/2022_10_20_123651_create_countries_table.php
+++ database/migrations/2022_10_20_123651_create_countries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateCountriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('country_uid')->unique();
+            $table->string('name')->unique();
+            $table->text('display_name')->index()->unique();
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('countries');
     }
 }
